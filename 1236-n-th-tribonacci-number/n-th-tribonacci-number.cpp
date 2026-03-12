30a01b4184c355e99b3f8244799b6b8648af2aed
PRASANJIT ROY <roy2402011@gmail.com>
2026-03-13 00:07:11 +0600

    Time: 0 ms (100.00%) | Memory: 7.7 MB (84.74%) - LeetSync

---
+++ 1236-n-th-tribonacci-number/n-th-tribonacci-number.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int dp[1005];
+
+    int fibo(int n)
+    {
+        if(n == 0) return 0;
+        if(n == 1 || n == 2) return 1;
+
+        if(dp[n] != -1)
+            return dp[n];
+
+        dp[n] = fibo(n-1) + fibo(n-2) + fibo(n-3);
+
+        return dp[n];
+    }
+
+    int tribonacci(int n) {
+        memset(dp, -1, sizeof(dp));
+        return fibo(n);
+    }
+};
