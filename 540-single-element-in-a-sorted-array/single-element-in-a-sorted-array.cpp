051ec9bcfcff3252037bacff60278d6c7eef24b3
PRASANJIT ROY <roy2402011@gmail.com>
2026-03-30 22:01:33 +0600

    Time: 0 ms (100.00%) | Memory: 26.1 MB (68.09%) - LeetSync

---
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int singleNonDuplicate(vector<int>& nums) {
+        int n = nums[0];
+        for(int i = 1; i < nums.size(); i++)
+        {
+            n ^= nums[i];
+        }
+        return n;
+    }
+};
